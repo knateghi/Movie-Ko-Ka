6692cc1f97b73aa10929571640a4537a8d284cb8
Kobra Nateghi <nateghikoby@gmail.com>
2020-06-23 21:11:35 -0400

    Initial Commit

---
+++ Movie Ko Ka/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Movie Ko Ka
+//
+//  Created by Kobra nateghi on 2020-06-23.
+//  Copyright © 2020 Kobra nateghi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
